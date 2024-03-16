ec2c8f420f4f94e1a1e29299baedce5f915b1b1a
Stanislau Liatsko <zysa2017@gmail.com>
2024-03-17 00:38:05 +0100

    Modified Task 10 and Done Task 11

--- Scala/lab04/src/main/scala/zad_10.scala
+++ Scala/lab04/src/main/scala/zad_10.scala
@@ -1,15 +1,15 @@
 def sum(l: List[Elem]): Elem = {
     @annotation.tailrec 
-    def helper(lista: List[Elem], acc: Elem = None): Elem = lista match {
-      case Some(some) :: reszta if some > 0 => 
+    def helper(list: List[Elem], acc: Elem = None): Elem = list match {
+      case Some(some) :: other if some > 0 => 
 
         val newAcc = acc match {
           case Some(number) => Some(number + some)
           case None => Some(some) 
         }
-        helper(reszta, newAcc)
+        helper(other, newAcc)
 
-      case _ :: reszta => helper(reszta, acc)
+      case _ :: other => helper(other, acc)
       case _ => acc
     }
       
@@ -19,7 +19,7 @@ def sum(l: List[Elem]): Elem = {
 type Elem = Option[Double]
 
 @main 
-def mainProg: Unit = {
+def task_10: Unit = {
   val l = List(Some(2.5), None, None, Some(1.5), Some(-1.5))
   println(sum(l))
 }
--- Scala/lab04/src/main/scala/zad_11.scala
+++ Scala/lab04/src/main/scala/zad_11.scala
@@ -1,25 +1,26 @@
-def max(l1: List[Double], l2: List[Double]): List[Double] = {
-    @annotation.tailrec 
-    def helper(l1, l2): Elem = lista match {
-      case Some(some) :: reszta if some > 0 => 
+def listMax(l1: List[Double], l2: List[Double]): List[Double] =  {
+  @annotation.tailrec 
+  def helper(l1: List[Double], l2: List[Double], result: List[Double] = Nil) : List[Double] = (l1, l2) match {
+    case (some1 :: other1, some2 :: other2) => 
+      if (some1 >= some2) {
+        helper(other1, other2, result :+ some1)
+      } else { 
+        helper(other1, other2, result :+ some2)
+      }
+    case (Nil , some) => result ++ some
+    case (some, Nil) => result ++ some
+  }
 
-        val newAcc = acc match {
-          case Some(number) => Some(number + some)
-          case None => Some(some) 
-        }
-        helper(reszta, newAcc)
-
-      case _ :: reszta => helper(reszta, acc)
-      case _ => acc
-    }
-      
-    helper(l)
+  helper(l1, l2)
 }
 
 @main 
-def mainProg: Unit = {
-    val list1 = List(2.5, 6.0, 15.0, 8.5)
-    val list2 = List(4.0, -3.0, 7.0, 5.4)
+def task_11: Unit = {
+    val list1 = List(2.0, -1.6, 3.2, 5.4, -8.4)
+    val list2 = List(3.3, -3.1, 3.2, -4.1, -0.4, 5.5)
+    val list3 = List()
+    println(listMax(list1, list2))
+    println(listMax(list3, list3))
 
-    println(max(list1, list2))
+
 }
